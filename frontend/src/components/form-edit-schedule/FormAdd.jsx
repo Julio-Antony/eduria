d5811e3a8a448f4a8b1cf3630f7e3b1ffb0d056c
Julio Antony <julioantony2@gmail.com>
2022-01-01 19:06:30 +0700

    Add functionality for form add schedule

--- frontend/src/components/form-edit-schedule/FormAdd.jsx
+++ frontend/src/components/form-edit-schedule/FormAdd.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useCallback, useEffect, useState } from "react";
 import Select, { components } from "react-select";
+import { useForm } from "react-hook-form";
+import swal from "sweetalert";
 import { getToken } from "../../config/Api.jsx";
 import "./form.css";
 
@@ -8,7 +10,16 @@ const FormAdd = () => {
   const [kelas, setKelas] = useState("");
   const [hari, setHari] = useState("");
   const [waktu, setWaktu] = useState("");
+  const [mapel, setMapel] = useState("");
+  const [id, setId] = useState("");
   const [classList, setClassList] = useState([]);
+  const [subjectList, setSubjectList] = useState([]);
+  const [submit, setSubmit] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const Placeholder = (props) => {
     return <components.Placeholder {...props} />;
@@ -18,17 +29,23 @@ const FormAdd = () => {
 
   const getData = useCallback(() => {
     const kelasUrl = "/api/class";
+    const subjectUrl = "/api/subject";
 
     const kelas = axios.get(kelasUrl, {
       headers: { Authorization: `Bearer ${token}` },
     });
 
+    const subject = axios.get(subjectUrl, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
     axios
-      .all([kelas])
+      .all([kelas, subject])
       .then(
         axios.spread((...allData) => {
-          console.log(allData[0].data.kelas);
+          console.log(allData[1].data.subject);
           setClassList(allData[0].data.kelas);
+          setSubjectList(allData[1].data.subject);
         })
       )
       .catch((err) => {
@@ -42,6 +59,10 @@ const FormAdd = () => {
 
   let kelasOptions = classList.map(function (kelas) {
     return { value: kelas.nama_kelas, label: kelas.nama_kelas };
+  });
+
+  let subjectOptions = subjectList.map(function (subject) {
+    return { value: subject.nama_mapel, label: subject.nama_mapel };
   });
 
   const hariOptions = [
@@ -60,12 +81,59 @@ const FormAdd = () => {
     { value: "13:10 - 14:30", label: "13:10 - 14:30" },
   ];
 
+  const onSubmit = () => {
+    setSubmit(true);
+    const data = {
+      kelas: kelas,
+      hari: hari,
+    };
+
+    const sesi = {
+      kelas: kelas,
+      waktu: waktu,
+      mapel: mapel,
+    };
+
+    axios
+      .post("/api/schedule", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res.data.createdSchedule._id);
+        axios
+          .post(`/api/schedule/${res.data.createdSchedule._id}/session`, sesi, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          })
+          .then((res) => {
+            console.log(res);
+            setSubmit(false);
+            swal("Jadwal ditambahkan", {
+              icon: "success",
+            });
+          })
+          .catch((err) => {
+            setSubmit(false);
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        setSubmit(false);
+        console.log(err);
+      });
+  };
+
   return (
     <div className="card full-height">
       <div className="card__header">
         <h3 className="mb-3">Buat Jadwal</h3>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group">
           <label for="exampleInputEmail1">Kelas</label>
           <Select
@@ -114,12 +182,12 @@ const FormAdd = () => {
             placeholder={"Mata Pelajaran"}
             maxMenuHeight={135}
             isSingle
-            // options={genderOptions}
-            // onChange={(e) => setGender(e.value)}
+            options={subjectOptions}
+            onChange={(e) => setMapel(e.value)}
           />
         </div>
         <button type="submit" className="btn btn-form">
-          Submit
+          Buat
         </button>
       </form>
     </div>
