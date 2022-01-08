530ea6c6ae3b072223d467b793387645bca91bc1
Julio-Antony <julioantony2@gmail.com>
2022-01-08 21:10:25 +0700

    add log activity functionality

--- frontend/src/components/table/TableSchedule.jsx
+++ frontend/src/components/table/TableSchedule.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 
-const TableSchedule = (props) => {
+const TableSchedule = () => {
   const columns = [
     {
       name: "Kelas",
